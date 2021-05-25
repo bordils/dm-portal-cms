4db890b8daced3049b0b7ce5571a664dac67f444
miki at NEEEU Spaces GmbH <>
2021-05-25 21:30:54 +0200

    changing to port 8080

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     //host: env("HOST", "0.0.0.0"),
     url: env("STRAPI_URL"),
-    port: env.int("PORT", 1337),
+    port: env.int("PORT", 8080),
     admin: {
       auth: {
         secret: env("ADMIN_JWT_SECRET", "ccee8e34019fdb86fb8552c899ed9075"),
